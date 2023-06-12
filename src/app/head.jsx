97b49f823875fbb29c81ce9e7522fcd9050731d1
Grigorios Papadopoulos <pagr1011@h-ka.de>
2023-06-12 19:44:52 +0200

    kleineanderung

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Buchweb</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This is a book search engine" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
