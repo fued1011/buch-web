7aee1b0c9055692e62526b83e18f6209b7cf9243
Grigorios Papadopoulos <pagr1011@h-ka.de>
2023-06-15 17:54:02 +0200

    Anpassungen der Dateien

---
+++ src/app/buch/page.tsx
@@ -0,0 +1,7 @@
+export default function Buch(){
+  return (
+    <main>
+      <h1>Buch Website</h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home(){
+import React from 'react';
+
+export default function Home() {
   return (
     <main>
-      <h1>Hello Webteam</h1>
+      <h1>Hallo Jungs</h1>
+      <p>Willkommen!</p>
+      <p>
+        Wir müssen das Ding so schnell wie nur möglich fertig bekommen
+      </p>
+      <button>Aufgehts</button>
     </main>
-  )
+  );
 }
